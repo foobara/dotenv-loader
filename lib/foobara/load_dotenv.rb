e86801b12c977d8e4aeae02f9c4db35ee4ebf6dc
Miles Georgi <azimux@gmail.com>
2024-01-10 21:10:35 +0000

    Initial commit, empty gem created with RubyMine

---
+++ lib/foobara/load_dotenv.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "load_dotenv/version"
+
+module Foobara
+  module LoadDotenv
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/foobara/load_dotenv/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Foobara
+  module LoadDotenv
+    VERSION = "0.1.0"
+  end
+end
