67aaa1865a5342a2a5845195f3efb0e98ce93e43
Miles Georgi <azimux@gmail.com>
2024-01-10 21:32:49 +0000

    Delete failing sample spec

--- spec/foobara/load_dotenv_spec.rb
+++ spec/foobara/load_dotenv_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Foobara::LoadDotenv do
   it "has a version number" do
     expect(Foobara::LoadDotenv::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
