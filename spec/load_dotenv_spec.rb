e7e0ee24bdc3806dbbf99a451a31b35486e0f15c
Miles Georgi <azimux@gmail.com>
2024-01-12 17:33:12 -0800

    Add spec for no .env files present

--- spec/load_dotenv_spec.rb
+++ spec/load_dotenv_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe Foobara::LoadDotenv do
+  let(:dir) { "#{__dir__}/fixtures/test_dotenvs" }
+
   it "has a version number" do
     expect(Foobara::LoadDotenv::VERSION).to_not be_nil
   end
@@ -12,7 +14,7 @@ RSpec.describe Foobara::LoadDotenv do
       let(:env) { "development" }
 
       it "applies .env.development.local" do
-        described_class.run!(env:, dir: "#{__dir__}/fixtures/test_dotenvs")
+        described_class.run!(env:, dir:)
 
         expect(Dotenv).to have_received(:load!).with(
           ".env.development.test.local",
@@ -22,6 +24,26 @@ RSpec.describe Foobara::LoadDotenv do
           ".env.development",
           ".env"
         )
+      end
+
+      context "when no .env files" do
+        let(:dir) { "#{__dir__}/fixtures/no_dotenvs" }
+
+        it "does nothing" do
+          expect {
+            described_class.run!(env:, dir:)
+          }.to_not raise_error
+        end
+      end
+
+      context "when no root .env file but there are others" do
+        let(:dir) { "#{__dir__}/fixtures/no_root_dotenv" }
+
+        it "does not try to load .env" do
+          described_class.run!(env:, dir:)
+
+          expect(Dotenv).to have_received(:load!).with(".env.development.test")
+        end
       end
     end
   end
